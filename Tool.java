c932beb765eba508375592132dc735013a31916a
Jonathan <jonjian@ucdavis.edu>
2017-10-10 18:02:32 -0700

    Added choices

--- Tool.java
+++ Tool.java
@@ -3,18 +3,35 @@ import java.util.Scanner;
 
 public class Tool{
 	public static void main(String[] args) throws IOException{
-		Tool tool = new Tool("test.txt");
-		tool.removeCarriageReturn();
+		System.out.println("Enter file name: ");
+		Scanner in = new Scanner(System.in);
+		String filename = in.nextLine();
+		Tool tool = new Tool(filename);
+		System.out.println("Choose a thing to do: ");
+		System.out.println("1: Remove \\r from newlines");
+		System.out.println("2: Print file as ascii values");
+		String thingToDo = in.nextLine();
+		if (thingToDo.length() != 1 || thingToDo.charAt(0) < 49 || thingToDo.charAt(0) > 57){
+			System.out.println("Not a valid option");
+			System.exit(0);
+		}
+		int thing = Integer.parseInt(thingToDo);
+		switch (thing){
+			case 1:
+				tool.removeCarriageReturn();
+				break;
+			case 2:
+				tool.printAscii();
+				break;
+		}
 		tool.writer.close();
 	}
 	
 	FileReader reader;
-	Scanner scanner;
 	FileWriter writer;
 	
 	public Tool(String fileName) throws IOException, FileNotFoundException{
 		reader = new FileReader(fileName);
-		scanner = new Scanner(reader);
 		writer = new FileWriter("new" + fileName);
 	}
 	
@@ -39,6 +56,7 @@ public class Tool{
 	}
 	
 	public void printAscii() throws IOException{
+	System.out.println("Printing Ascii values");
 		int lastRead = reader.read();
 		while (lastRead > 0){
 			System.out.println(lastRead);
